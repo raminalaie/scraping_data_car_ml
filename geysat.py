3739f7bd3cdc8097e9dd76199f25a167152403f3
raminalaie.79@gmail.com <109432088+raminalaie@users.noreply.github.com>
2022-11-27 13:02:36 +0330

    add ml algoritm tree

--- geysat.py
+++ geysat.py
@@ -2,19 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import mysql.connector
-
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
 vorodi = input("data beroz shavad?y/n = ")
 
 if vorodi == "y":
       driver = webdriver.Firefox()
-      url = "https://bama.ir/car/all/isfahan-isfahan?year=1397,1401&price=0,950000000&image=1&country=iranian"
+      url = "https://bama.ir/car/peugeot-pars-mt?price=0,950000000&mileage=1000&image=1&priced=1"
       driver.get(url)
       #scorol web
-      SCROLL_PAUSE_TIME = 2
+      SCROLL_PAUSE_TIME = 6
       last_height = driver.execute_script("return document.body.scrollHeight")
       while True:
             driver.execute_script("window.scrollTo(0, (document.body.scrollHeight-400));")
             time.sleep(SCROLL_PAUSE_TIME)
+            
             new_height = driver.execute_script("return document.body.scrollHeight")
             if new_height == last_height:
                   break
@@ -28,10 +30,11 @@ if vorodi == "y":
       list_price = []
       list_kilometr = []
       list_sal = []
+      
       for i in kilometer:
             kilometers.append(i.text.replace("\n",",").split(","))
       for i in kilometers:
-            list_kilometr.append(i[1])
+            list_kilometr.append(int(i[1]))
       for i in kilometers:
             list_sal.append(int(i[0]))
       for i in name:
@@ -47,7 +50,7 @@ if vorodi == "y":
             database = "learn"
             )      
             mycursor = mydb.cursor()      
-            mycursor.execute("TRUNCATE TABLE listname")
+            mycursor.execute("TRUNCATE TABLE listnameml")
             mydb.close()
       #add new data in table sql 
       def create(list_name,list_price,list_kilometr,list_sal):
@@ -64,12 +67,17 @@ if vorodi == "y":
                   price = list_price[i]
                   kilome = list_kilometr[i]
                   sale = list_sal[i]
-                  mycursor.execute("INSERt INTO listnameml VALUES (\"%s\", \"%s\" , %i , \"%i\")" % (Name,kilome,sale,price))
+                  mycursor.execute("INSERt INTO listnameml VALUES (\"%s\", \"%i\" , %i , \"%i\")" % (Name,kilome,sale,price))
                   mydb.commit()
             mydb.close()
       create(list_name,list_price,list_kilometr,list_sal)
 elif vorodi == "n":
-      def data_ml():
+      
+      name_car = input("name [فقط پژو پارس] = ")
+      karkerd = int(input("karkerd be hezar [20,115مثل] ="))
+      salesakht = int(input("sale sakht [مثل1389,1400] = "))
+      
+      def data_ml(name_car,karkerd,salesakht):
             mydb = mysql.connector.connect(    
             user="root",
             password="",
@@ -82,17 +90,24 @@ elif vorodi == "n":
             x = []
             y = []
             for (Name,kilome,sale,price) in mycursor:
-                  p=[Name,kilome,sale]
+                  p=[kilome,sale]
                   x.append(p)         
                   y.append(price)
+                  
+            clf = tree.DecisionTreeClassifier()
+            clf = clf.fit(x,y)
             
-            for  i in range(len(x)):
-                  print(x[i],"   ",y[i])      
-
+            new_data = [[karkerd,salesakht]]
+            answer = clf.predict(new_data)
+            
+            print("=============================================")
+            print("geymat mashin = ",answer[0])
+            
             mydb.close()
+            
+      data_ml(name_car, karkerd,salesakht)
 
-      data_ml()
-
+      
 
 
 
